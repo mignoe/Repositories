379ec7db082b9e134d838f30413b961ae7669b40
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2024-09-27 15:08:10 -0300

    making changes

---
+++ src/components/repository-forks/repository-forks.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RepositoryForksComponent } from './repository-forks.component';
+
+describe('RepositoryForksComponent', () => {
+  let component: RepositoryForksComponent;
+  let fixture: ComponentFixture<RepositoryForksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RepositoryForksComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(RepositoryForksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/components/repository-forks/repository-forks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-forks',
+  standalone: true,
+  imports: [],
+  templateUrl: './repository-forks.component.html',
+  styleUrl: './repository-forks.component.css'
+})
+export class RepositoryForksComponent {
+
+}
