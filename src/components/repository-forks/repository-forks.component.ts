347cd0101b8d25db2b8fdc082ddb30bf8b4f0e0c
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2024-09-28 22:47:13 -0300

    making the forks requisition:  - Now our application is able to get the forks made from a repository and display some of their informations on the page  - Now we need to create filter and other features to organize the results

--- src/components/repository-forks/repository-forks.component.ts
+++ src/components/repository-forks/repository-forks.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { distinct, orderBy } from '../../utils';
+import { Fork, Owner } from '../../classes';
+
+import { NgFor, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-repository-forks',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor, CommonModule],
   templateUrl: './repository-forks.component.html',
   styleUrl: './repository-forks.component.css'
 })
 export class RepositoryForksComponent {
   repository = "";
+  forks: Fork[] = [];
 
   constructor() {
     // this.repository = "angular/angular";
@@ -18,6 +23,22 @@ export class RepositoryForksComponent {
 
   search() {
     console.log(this.repository);
+    const url = "https://api.github.com/repos/" + this.repository + "/forks" // your API URL
+
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Fetched data:", data);
+        this.forks = distinct(data, "id");
+      })
+      .catch(error => {
+        console.error("Error fetching the URL:", error);
+      });
+  }
+
+  orderForksBy(field : keyof Fork) {
+    this.forks = orderBy(this.forks, field);
   }
 
 }
