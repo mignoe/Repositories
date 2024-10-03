c1b578dd20d7bd36b719e97133caaf2558cb72fe
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2024-10-03 17:52:05 -0300

    modifying utils again

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ export const distinct = <T>(colecao: T[], atributo: keyof T): T[] =>
     [...new Map(colecao.map(item => [item[atributo], item])).values()];
 
 
+export const fold = <T, U>(reducer: (acc: U, item: T) => U, init: U, array: T[]): U => 
+  array.length != 0 ? fold(reducer, reducer(init, array[0]), array.slice(1)) : init;
+
+
 export const groupBy = <T>(colecao: T[], atributo: keyof T): { [key: string]: T[] } =>
     colecao.reduce((acc, obj) => {
       const key = obj[atributo] as unknown as string;
@@ -15,10 +19,8 @@ export const groupBy = <T>(colecao: T[], atributo: keyof T): { [key: string]: T[
 export const orderBy = <T>(colecao: T[], atributo: keyof T): T[] => 
 [...colecao].sort((a, b) => (a[atributo] > b[atributo] ? 1 : -1));
 
-const fold = <T, U>(reducer: (acc: U, item: T) => U, init: U, array: T[]): U => 
-  array.length != 0 ? fold(reducer, reducer(init, array[0]), array.slice(1)) : init;
 
-export const compose = <T>(f1: (arg: T) => T, f2: (arg: T) => T) => (arg: T): T => 
-f1(f2(arg));
+export const compose = (...fns: Function[]) => (arg: any) =>
+  fns.reduceRight((acc, fn) => fn(acc), arg);
 
   
